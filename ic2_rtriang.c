16ae9889345614958f4415a1688dfe7fb301952a
PravdaPalatus <63147444+PravdaPalatus@users.noreply.github.com>
2022-03-01 10:57:42 +0100

    Update ic2_rtriang.c

--- ic2_rtriang.c
+++ ic2_rtriang.c
@@ -1,15 +1,13 @@
 //Right Triangle
 #include <stdio.h>
-//para incluir a biblioteca de matemática
 #include <math.h>
 int main(){
     float a;
-    // o a e o b são float e não int como estávamos usando
     float b;
     printf("Valor de a: ");
     scanf("%f", &a);
     printf("Valor de b: ");
     scanf("%f", &b);
-    printf("O valor da hypotenusa é: %0.3f\n", sqrt(a*a + b*b)); //sqrt() está na biblio de math
+    printf("O valor da hypotenusa é: %0.3f\n", sqrt(a*a + b*b));
     return 0;
 }
