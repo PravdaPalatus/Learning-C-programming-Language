9148b61fbfbb5422b23f5fdde5a1defd68c41097
PravdaPalatus <63147444+PravdaPalatus@users.noreply.github.com>
2022-03-11 12:17:00 +0100

    Add files via upload

---
+++ odd_numbers.c
@@ -0,0 +1,26 @@
+//oddNumbers
+
+#include <stdio.h>
+
+void print_odds(int a, int b);
+
+int main(){
+
+    int a, b;
+    printf("Enter the range (a -> b) that you want to print the odds between (PS:a < b): \n");
+    scanf("%d %d", &a, &b);
+    print_odds(a, b);
+    return 0;
+}
+
+void print_odds(int a, int b){
+    for(int i = 0; i <= (b - a); i++){
+        if((a + i) % 2 != 0){
+            if(i == 0 || i == 1) printf("%d", a + i);
+            else{
+                printf(", %d", a + i);
+            }
+        }
+    }
+    printf("\n");
+}
