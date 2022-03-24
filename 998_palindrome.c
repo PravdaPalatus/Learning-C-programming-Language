390998438fdb1cd346185d3ff31e000b88eec7bb
PravdaPalatus <63147444+PravdaPalatus@users.noreply.github.com>
2022-03-24 11:08:09 +0100

    Update and rename palindrome.c to 998_palindrome.c

--- 998_palindrome.c
+++ 998_palindrome.c
@@ -1,7 +1,7 @@
 //palindrome
 
 #include <stdio.h>
-#include <string.h> //função para calcular o length of a string(array): strlen(str)
+#include <string.h>
 #define MAX_S 1000
 
 int string_length(char *msg);
@@ -10,14 +10,14 @@ int is_polin(char *msg);
 
 int main(){
     char msg[MAX_S]; msg[MAX_S - 1] = '\0';
-    printf("Qual a frase: ");
+    printf("Enter a string:");
     fgets(msg, MAX_S - 1, stdin); msg[strlen(msg) - 1] = '\0';
     space_out(msg);
     int res;
     res = is_polin(msg);
     printf("%d\n", res);
-    printf("Sua msg é polindrome?\n");
-    if(res == 1) printf("É um Polindrome!\n");
+    printf("It is a palindrome?\n");
+    if(res == 1) printf("Yes, it is a palindrome!\n");
     else printf("Is is not a Polindrome\n");
 
     
@@ -31,7 +31,7 @@ int string_length(char *msg){
     return c;
 }
 
-//remover os espaços do string
+
 void space_out(char *msg){ //pode usar: for(i=0;msg[i]!='\0';i++){if(msg[i]!='\0')msg[b]=msg[i]}
     int c = 0;
     while(msg[c] != '\0'){
@@ -47,7 +47,7 @@ void space_out(char *msg){ //pode usar: for(i=0;msg[i]!='\0';i++){if(msg[i]!='\0
             c++;
         }
     }
-    printf("Agora vai: %s\n", msg);
+    printf("String without spaces: %s\n", msg);
 }
 
 int is_polin(char *msg){
